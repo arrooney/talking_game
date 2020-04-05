6d3a301457954f5d3adf1e74e5a5e62acd884a0c
Andrew R. Rooney <Andrew_Rooney@intuit.com>
2020-04-04 22:54:30 -0600

    starting the game logic

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 class Server {
     constructor() {
         this.express = require('express');
+        this.redis = require('redis');
+        this.client = this.redis.createClient(); //creates a new client
         this.app = this.express();
         this.server = require('http').Server(this.app);
         this.io = require('socket.io').listen(this.server);
@@ -14,16 +16,57 @@ class Server {
         this.server.listen(8081, () => { // Listens to port 80
             console.log('Listening on ' + this.server.address().port);
         });
+        this.client.on('connect', function() {
+            console.log('connected');
+        });
         this.setup();
     }
-    
+
     setup() {
         this.io.on("connection", (socket) => {
-            /*  RX EVENTS  */
-            socket.on("test", (data) => {
-                console.log(data);
-            });
+            this.socket = socket;
+            this.centralServer();
         });
+    }
+    
+    centralServer() {
+        /*  RX EVENTS  */
+        
+        this.socket.on("newRoomRequest", (data, callback) => {
+            this.Room = data;
+            this.newRoom(callback);
+        });
+
+        this.socket.on("joinRoomRequest", (data, callback) => {
+            this.Room = data;
+            this.joinRoom(callback);
+        });
+    }
+
+    joinRoom(callback) {
+        this.client.hgetall(this.Room.name, function(err, resp) {
+            if (resp === null) {
+                console.log("did not exist");
+                callback(false, null, "could not join room");
+            } else {
+                callback(true, resp, "joined room");
+            }
+        });
+    }
+
+    newRoom(callback) {
+        this.client.exists(this.Room.name, function(err, reply) {
+            if (reply === 1) {
+                console.log('exists');
+                callback(false, "room exists");
+                return;
+            } else {
+                console.log('doesn\'t exist');
+                this.client.hset(this.Room.name, "owner", this.socket.id);
+                this.client.hset(this.Room.name, "suggestions", this.Room.suggestions);
+                callback(true, "room has been created");
+            }
+        }.bind(this));
     }
 }
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,46 @@
 var app = angular.module("app", ['btford.socket-io']);
 
 app.controller('myGame', ['$scope', 'socketFactory', function($scope, socketFactory) {
-  $scope.greeting = "Asdfasdf";
-  var socket = socketFactory();
-  socket.emit("test", "my data");
+    class Player {
+        constructor() {
+            this.dom          = $scope;
+            this.dom.Room     = {};
+            this.dom.greeting = "The talking game 📢";
+            this.socket       = socketFactory();
+            this.roomExists   = false;
+            this.setup();
+        }
+
+        setup() {
+            // this.socket.emit("test", "my data");
+        }
+        
+        createGame() {
+            this.dom.greeting = "Creating game...";
+            this.socket.emit("newRoomRequest", this.dom.Room, (resp, msg) => {
+                console.log(msg);
+                if(resp) {
+                    this.dom.greeting = "Game created 🙌\nRoom name is \'" + this.dom.Room.name + "\'\nEach player gets " + this.dom.Room.suggestions + " suggestions.";
+                    this.dom.join = true;
+                } else {
+                    this.dom.greeting = "Room \'" + this.dom.Room.name + "\' already exists. Try a different name!";
+                    this.dom.create = true;
+                }
+                this.dom.Room = {};
+            });
+            this.dom.create = false;
+        }
+        
+        joinGame() {
+            this.socket.emit("joinRoomRequest", this.dom.Room, (resp, roomDetails, msg) => {
+                if (resp) {
+                    this.roomExists = true;
+                    this.joinMsg = "Enter your name to join \'" + this.dom.Room.name + "\'";
+                    console.log(roomDetails);
+                }
+            });
+            this.dom.join = false;
+        }
+    }
+    $scope.player = new Player();
 }]);
